7b72774951dc0deb8d199e200ebad5d3872abde7
billyeatcookies <billydevbusiness@gmail.com>
2021-06-08 12:04:36 +0300

    Initial Commit
    - added library project
    - added test project

---
+++ pastemyst/pastemyst.cpp
@@ -0,0 +1,19 @@
+// pastemyst.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include "pastemyst.h"
+
+Client::Client() {
+	
+}
+
+void Client::get_paste() {
+
+}
+
+void Client::get_private_paste() {
+
+}
---
+++ pastemyst/pastemyst.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Client {
+private:
+	// ...
+public:
+	Client();
+	void get_paste();
+	void get_private_paste();
+};
