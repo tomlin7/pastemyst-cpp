f2d2fc5344763c8459aebf4b7ff9a3d97cb548aa
billyeatcookies <billydevbusiness@gmail.com>
2021-06-09 12:07:53 +0300

    Added tests  for the implemented client class and it's paste  endpoint functions (not complete)
    - get paste
    - create paste
    - edit paste
    - delete paste

--- pastemyst.tests/pastemyst.tests.cpp
+++ pastemyst.tests/pastemyst.tests.cpp
@@ -1,20 +1,16 @@
-// pastemyst.tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "pastemyst.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Client client = Client();
+    client.Authorize("token");
+
+    std::cout << client.GetPaste("paste_id").text << std::endl;
+    std::cout << client.CreatePaste("{\"isPrivate\": true, \"pasties\": [{\"title\": \"new private paste\",\"language\": \"Autodetect\",\"code\": \"new private paste content\"}]}").text << std::endl;
+    std::cout << client.EditPaste("paste_id", "{\"pasties\": [{\"_id\": \"pasty_id\", \"title\": \"edited\",\"language\": \"Autodetect\",\"code\": \"edited paste content\"}]}").text << std::endl;
+    std::cout << client.DeletePaste("paste_id").text << std::endl;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
