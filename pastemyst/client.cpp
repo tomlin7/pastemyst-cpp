8fef164cfa6d9ce3f87fdbbf7c6cbf830b3293cb
billyeatcookies <billydevbusiness@gmail.com>
2021-06-10 11:30:11 +0300

    Moved Client to a separate module

---
+++ pastemyst/client.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "framework.h"
+
+#include "client.h"
+#include "endpoints.h"
+
+Client::Client() {
+	// ...
+}
+
+Client::Client(std::string auth_token) {
+	this->auth_token = auth_token;
+}
+
+void Client::Authorize(std::string auth_token) {
+	this->auth_token = auth_token;
+}
+
+//void Client::Get() {
+//
+//}
+//
+//void Client::Post() {
+//
+//}
+//void Client::Patch() {
+//
+//}
+//void Client::Delete () {
+//
+//}
--- pastemyst/pastemyst.h
+++ pastemyst/pastemyst.h
@@ -1,32 +1,6 @@
 #pragma once
 
-#include "cpr/cpr.h"
-
-class Client {
-private:
-	// ...
-	
-public:
-	std::string auth_token;
-
-	Client();
-	Client(std::string auth_token);
-
-    void Authorize(std::string auth_token);
-
-	cpr::Response GetPaste(std::string pasteID);
-	cpr::Response CreatePaste(std::string paste_content);
-	cpr::Response EditPaste(std::string pasteID, std::string editContent);
-	cpr::Response DeletePaste(std::string pasteID);
-
-	/*cpr::Response UserExists();
-	cpr::Response GetUser();
-
-	cpr::Response GetLanguage();
-	
-	cpr::Response ExpiresinToUnixTimestamp();*/
-};
-
+#include "client.h"
 
 
 //struct Pasty {
